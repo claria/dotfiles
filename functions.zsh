e0c996e26dcbf1efedc01267b3f4692854867d43
Georg Sieber <g.sieber@gmail.com>
2014-04-24 14:00:25 +0200

    deleted obsolete files

--- functions.zsh
+++
@@ -1,7 +0,0 @@
-#FastNLO Python bindings
-function setfnlo()
-{
-	FNLO="/home/aem/uni/sw/fnloreader/"
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$FNLO/lib
-	export PYTHONPATH=$PYTHONPATH:$FNLO/lib/python2.7/site-packages
-}
