f299de72ca703ebcee004b17e8ee4c9ae6beb00e
Georg Sieber <g.sieber@gmail.com>
2015-09-26 21:42:14 +0200

    rename files add github to gitconfig

--- bash/nafhh-cms0?/aliases.sh
+++ bash/nafhh-cms0?/aliases.sh
@@ -1,3 +1,5 @@
 alias screen='TERM=rxvt-256color screen'
 alias meld='/usr/bin/python /usr/bin/meld'
 alias lmeld='/usr/bin/python /usr/bin/meld'
+alias module='eval $(/usr/bin/modulecmd bash)'
+
--- bash/nafhh-cms0?/lhapdf.zsh
+++
@@ -1,7 +0,0 @@
-setlhapdf () {
-   LHAPDF=/nfs/dust/cms/user/gsieber/sw/lhapdf
-   export LHAPATH=/nfs/dust/cms/user/gsieber/lhgrids
-   export PATH=$LHAPDF/bin:$PATH
-   export LD_LIBRARY_PATH=$LHAPDF/lib/:$LD_LIBRARY_PATH
-   export PYTHONPATH=$LHAPDF/lib/python2.7/site-packages:$PYTHONPATH
-}
