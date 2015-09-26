19ebb32f876b9664c24f54a44aa23097da866035
Georg Sieber <g.sieber@gmail.com>
2015-09-26 21:40:29 +0200

    use newer git on naf.

---
+++ bash/nafhh-cms0?/lib.sh
@@ -0,0 +1,3 @@
+export PATH=/opt/git/1.9/bin:$PATH
+export PERL5LIB=/opt/git/1.9/share/perl5/vendor_perl:/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/git/1.8.3.1-cms3/share/perl5
+export _LMFILES_=/etc/modulefiles/git/1.9
